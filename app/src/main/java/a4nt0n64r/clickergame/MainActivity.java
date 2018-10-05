02d51266fba49291c7be94cc3c79e604808ba6b2
Anton Sharapov <cheel4069@gmail.com>
2018-10-05 21:00:14 +0200

    first worked version
    add button and textview with counter

--- app/src/main/java/a4nt0n64r/clickergame/MainActivity.java
+++ app/src/main/java/a4nt0n64r/clickergame/MainActivity.java
@@ -2,12 +2,34 @@ package a4nt0n64r.clickergame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStartButton;
+    private TextView mCounterTextView;
+
+    private int mCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mCounterTextView = (TextView) findViewById(R.id.counter_text_view);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCounter++;
+                mCounterTextView.setText("" + mCounter);
+            }
+        });
+
+
+
+
     }
 }
