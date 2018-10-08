edfd5a322fc93366cb34879ce388f8a4052ceac6
Anton Sharapov <cheel4069@gmail.com>
2018-10-08 19:18:44 +0200

    testing imageView and timer
    add imageView and timer to mainActivity, just for testing.

--- app/src/main/java/a4nt0n64r/clickergame/MainActivity.java
+++ app/src/main/java/a4nt0n64r/clickergame/MainActivity.java
@@ -1,15 +1,20 @@
 package a4nt0n64r.clickergame;
 
+import android.content.pm.ActivityInfo;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mStartButton;
+    private Button mTapButton;
     private TextView mCounterTextView;
+    private TextView mCountDown;
+    private ImageView mStart;
 
     private int mCounter = 0;
 
@@ -18,15 +23,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mStartButton = (Button) findViewById(R.id.start_button);
+        mTapButton = (Button) findViewById(R.id.start_button);
         mCounterTextView = (TextView) findViewById(R.id.counter_text_view);
-        mStartButton.setOnClickListener(new View.OnClickListener() {
+        mCountDown = (TextView) findViewById(R.id.countDown);
+
+        mTapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mCounter++;
                 mCounterTextView.setText("" + mCounter);
             }
         });
+
+        mStart = (ImageView) findViewById(R.id.startImage);
+
+        mStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                new CountDownTimer(60000, 1000) {
+
+                    public void onTick(long l) {
+                        mCountDown.setText("" + l/ 1000);
+                    }
+
+                    public void onFinish() {
+                        mCountDown.setText("-");
+                    }
+                }.start();
+
+            }
+       });
+
 
 
 
