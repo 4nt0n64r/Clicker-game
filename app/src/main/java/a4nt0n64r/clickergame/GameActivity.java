8cf91a952be3650caffd5a73b8e02048eeee577a
Anton Sharapov <cheel4069@gmail.com>
2018-10-10 00:13:14 +0200

    Make redesign of app
    Add stylish buttons. Add string for endgame congratulations.

--- app/src/main/java/a4nt0n64r/clickergame/GameActivity.java
+++ app/src/main/java/a4nt0n64r/clickergame/GameActivity.java
@@ -1,5 +1,6 @@
 package a4nt0n64r.clickergame;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,28 +12,42 @@ public class GameActivity extends AppCompatActivity {
 
     private Button mTapButton;
     private TextView mTimer;
-    private TextView mPoints;
+    private TextView mPointsTW;
+
+    private int mEarnedPoints = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        mTapButton = (Button) findViewById(R.id.tap_button);        //прикрепить дисплей из хмл
+        mTapButton = (Button) findViewById(R.id.tap_button);
+        mTimer = (TextView) findViewById(R.id.timer);
+        mPointsTW = (TextView) findViewById(R.id.points);
 
-        mTapButton.setOnClickListener(new View.OnClickListener(){
+        new CountDownTimer(10000, 1000) {
+
+            public void onTick(long l) {
+                mTimer.setText("" + l / 1000);
+            }       //высунуть таймер из нажатия
+
+            public void onFinish() {
+                mTimer.setText("0");
+                // сообщение "время вышло"
+                //стартуем итоговую активность
+                Intent intent = new Intent(GameActivity.this, EndActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+
+
+        mTapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mEarnedPoints++;
+                mPointsTW.setText(""+mEarnedPoints);
 
-                new CountDownTimer(60000, 1000) {
-
-                    public void onTick(long l) {
-                        mTimer.setText("" + l/ 1000);
-                    }
-
-                    public void onFinish() {
-                        mTimer.setText("-");
-                    }
-                }.start();
+            }
+        });
     }
 }
--- app/src/main/java/a4nt0n64r/clickergame/MainActivity.java
+++ app/src/main/java/a4nt0n64r/clickergame/MainActivity.java
@@ -1,10 +1,12 @@
 package a4nt0n64r.clickergame;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                //стартуем игровую активность
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
             }
         });
 
